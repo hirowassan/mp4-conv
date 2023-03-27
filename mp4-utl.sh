a43957e0a74b8a2b42d42f008f0c582f3b7267e7
hirowassan <98885856+hirowassan@users.noreply.github.com>
2023-03-28 03:26:56 +0900

    Update mp4-utl.sh

--- mp4-utl.sh
+++ mp4-utl.sh
@@ -5,6 +5,11 @@ if command -v ffmpeg &>/dev/null; then
     if [ ! -d "output" ]; then
         mkdir output
     fi
+    cd output
+    if [ ! -d "frames" ]; then
+        mkdir frames
+    fi
+    cd -
 	echo ' _   _ _'
 	echo '| | | (_)_ __ _____      ____ _ ___ ___  __ _ _ __'
 	echo '| |_| | | '\''_// _ \ \ /\ / / _` / __/ __|/ _` | '\''_ \'
@@ -37,28 +42,33 @@ if command -v ffmpeg &>/dev/null; then
     	echo "1. mp4→gif"
     	echo "2. mp4→mp3"
     	echo "3. mp4を無音化"
-    	echo "4. 終了"
+	echo "4. mp4をpngで連番"
+    	echo "5. 終了"
 
     	read -p "選択: " OPTION
 
     	if [ "$OPTION" = "1" ]; then
-        	ffmpeg -i "$FILE" ./output/$OUTPUT.gif
+        	ffmpeg -i "$FILE" -c ./output/$OUTPUT.gif
 		clear
 		echo "outputに$OUTPUT.gifが出力されました!"
 		break
     	elif [ "$OPTION" = "2" ]; then
-        	ffmpeg -i "$FILE" ./output/$OUTPUT.mp3
+        	ffmpeg -i "$FILE" -c ./output/$OUTPUT.mp3
 		clear
 		echo "outputに$OUTPUT.mp3が出力されました!"
 		break
     	elif [ "$OPTION" = "3" ]; then
-        	ffmpeg -i "$FILE" -an ./output/$OUTPUT.mp4
+        	ffmpeg -i "$FILE" -c -an ./output/$OUTPUT.mp4
 		clear
 		echo "outputに$OUTPUT.mp4が出力されました!"
 		break
     	elif [ "$OPTION" = "4" ]; then
+		ffmpeg -r 1 -i $FILE ./output/frames/$OUTPUT%05d.png
 		clear
         	break
+	elif [ "$OPTION" = "5"]; then
+		clear
+		break
     	else
         	echo "無効な選択肢です。"
 	    fi
